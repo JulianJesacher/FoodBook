185d6a608d7f2600c3b79d253e6843e40d83bc15
Julian <julianjesacher@gmail.com>
2022-08-05 16:05:59 +0200

    Add continue as guest option

--- apps/frontend/src/app/tools/header/header.component.ts
+++ apps/frontend/src/app/tools/header/header.component.ts
@@ -1,23 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
-import { AuthenticatorServiceInterface } from '../../services/authService/authenticator.service';
+import { Router } from '@angular/router';
+import { AuthenticatorServiceInterface, AuthService } from '../../services/authService/authenticator.service';
 import { AuthenticatorComponent } from '../authenticator/authenticator.component';
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+    constructor(
+        private loginSheet: MatBottomSheet,
+        public authService: AuthenticatorServiceInterface,
+        private router: Router,
+    ) {}
 
-  constructor(private loginSheet: MatBottomSheet,
-    public authService: AuthenticatorServiceInterface) { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
+    onLoginClick() {
+        this.loginSheet.open(AuthenticatorComponent);
+    }
 
-  }
-
-  onLoginClick() {
-    this.loginSheet.open(AuthenticatorComponent)
-  }
-
+    guestLogin() {
+        this.authService.login({ email: 'Guest', password: 'guestAccountPassword' }).subscribe({
+            next: () => {
+                this.router.navigateByUrl('/home');
+            },
+        });
+    }
 }
